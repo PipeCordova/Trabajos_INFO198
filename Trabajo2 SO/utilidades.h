afefd9ffbc4acb34f88aad6319d353e46add63a5
Felipe Córdova <85969736+PipeCordova@users.noreply.github.com>
2023-08-22 17:29:29 -0400

    actualizado

--- Trabajo2 SO/utilidades.h
+++ Trabajo2 SO/utilidades.h
@@ -9,7 +9,7 @@ using namespace std;
 bool usuarioExiste(const string& nombreUsuario, const string& nombreArchivo, vector<int>& permisos);
 vector<int> convertirlo(const string& v);
 int calcularModa(const vector<int>& numeros);
-void crearArchivo(const string& nombreArchivo);
+void crearArchivo(const string& nombreArchivo, const string &contenido);
 void agregarLineaArchivo(const string& nombreArchivo, const string& linea);
 
 #endif // UTILIDADES_H
