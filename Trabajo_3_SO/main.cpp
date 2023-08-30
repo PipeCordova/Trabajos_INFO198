706e0b279490fa88494f6a85b09fe10b8c83ac5f
Felipe Córdova <85969736+PipeCordova@users.noreply.github.com>
2023-08-30 15:02:37 -0400

    codigo listo

---
+++ Trabajo_3_SO/main.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <unistd.h>
+#include <algorithm>
+#include <numeric>
+#include "metodos.h"
+#include <chrono>
+#include <thread> 
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    string u, v, f, t, i, o;
+    int c;
+    while ((c = getopt(argc, argv, "u:v:f:t:i:o:")) != -1) {
+        switch (c) {
+            case 'u':
+                u = optarg;
+                break;
+            case 'v':
+                v = optarg;
+                break;
+            case 'f':
+                f = optarg;
+                break;
+            case 't':
+                t = optarg;
+                break;
+            case 'i':
+                i = optarg;
+                break;
+            case 'o':
+                o = optarg;
+                break;
+        }
+    }
+
+    
+    // rutaPermisos = BD.txt , rutaMenu = menu.txt , rutaPerfiles = perfilesUsuarios.txt
+    string rutaPermisos, rutaMenu, rutaPerfiles;
+    leerConfiguracion(rutaPermisos, rutaMenu, rutaPerfiles);
+
+    
+    // Aqui se guarda string que corresponde al permiso del usuario, osea: admin, userGeneral, userCookie.
+    string perfil = obtenerPermisosDesdeArchivo(rutaPermisos, u);
+    vector<int> vectorEntrada = convertirlo(v); // Convertir a vector el "vector" v de la entrada.
+
+
+    cout << "\n- Usuario: " << u << endl;
+    cout << "- Perfil:" << perfil << endl << endl;
+    
+    vector<int> admin = obtenerVectorDesdeLinea(1, rutaPerfiles);
+    vector<int> userGeneral = obtenerVectorDesdeLinea(2, rutaPerfiles);
+    vector<int> userRokie = obtenerVectorDesdeLinea(3, rutaPerfiles);
+
+
+    while (true) {
+        ifstream archivo(rutaMenu);
+        ifstream archivoTexto(f);
+        ifstream archivoSalida(o);
+
+        cout << "Opciones disponibles:" << endl;
+        string linea;
+        while (getline(archivo, linea)) {
+            cout << linea << endl;
+        }
+
+        int eleccion = obtenerEleccion();
+
+        if (eleccion == 0) {
+            break;
+        } 
+        if (eleccion >= 8) {
+            cout << "Opcion " << eleccion << " aun no ha sido implementada!" << endl << endl;
+        } else { // si no es >= 8 entonces si o si esta entre 1 y 7
+            if (perfil == " admin") {
+                ejecutarOpcion(eleccion, admin, vectorEntrada, archivoTexto, f, t, i, o, archivoSalida);
+            }
+            if (perfil == " userGeneral") {
+                ejecutarOpcion(eleccion, userGeneral, vectorEntrada, archivoTexto, f, t, i, o, archivoSalida);
+            }
+            if (perfil == " userRookie") {
+                ejecutarOpcion(eleccion, userRokie, vectorEntrada, archivoTexto, f, t, i, o, archivoSalida);
+            }
+        }
+
+
+        cout << "Espere 5 segundos!!\n" << endl;
+        this_thread::sleep_for(chrono::seconds(5)); // Esperar 5 segundos y se limpia la consola
+        system("clear");
+    }
+
+    agregarElementosBD(rutaPermisos);
+    cout << "Trabajo 3 terminado!!\n" << endl;
+    return EXIT_SUCCESS;
+}
---
+++ Trabajo_3_SO/procesarCuento.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+#include "metodos.h"
+
+using namespace std;
+
+// Función para convertir el texto en una lista de palabras
+vector<string> dividirTexto(const string& texto) {
+    vector<string> palabras;
+    istringstream iss(texto);
+    string palabra;
+    
+    while (iss >> palabra) {
+        // Limpieza básica para eliminar signos de puntuación
+        palabra.erase(remove_if(palabra.begin(), palabra.end(), ::ispunct), palabra.end());
+        palabras.push_back(palabra);
+    }
+    
+    return palabras;
+}
+
+// Función de bucket sort para ordenar las palabras por frecuencia
+void ordenarPorFrecuencia(vector<pair<string, int>>& paresPalabraFrecuencia) {
+    const int numBuckets = 10; // Puedes ajustar el número de buckets según tu preferencia
+    
+    vector<vector<pair<string, int>>> buckets(numBuckets);
+    
+    for (const auto& par : paresPalabraFrecuencia) {
+        int indiceBucket = min(par.second / numBuckets, numBuckets - 1);
+        buckets[indiceBucket].push_back(par);
+    }
+    
+    paresPalabraFrecuencia.clear();
+    
+    for (auto& bucket : buckets) {
+        sort(bucket.begin(), bucket.end(), [](const auto& a, const auto& b) {
+            return a.second > b.second;
+        });
+        
+        for (const auto& par : bucket) {
+            paresPalabraFrecuencia.push_back(par);
+        }
+    }
+}
+
+void procesarArchivo(const string & i, const string & o) {
+    string rutaArchivoEntrada = i; // Actualiza con la ruta correcta
+    string rutaArchivoSalida = o; // Actualiza con la ruta correcta
+    
+    ifstream archivoEntrada(rutaArchivoEntrada);
+    if (!archivoEntrada.is_open()) {
+        cerr << "No se pudo abrir el archivo de entrada.\n" << endl;
+        exit(EXIT_FAILURE);
+    }
+    
+    unordered_map<string, int> mapaFrecuenciaPalabra;
+    string linea;
+    
+    // Leer el archivo línea por línea y contar las palabras
+    while (getline(archivoEntrada, linea)) {
+        vector<string> palabras = dividirTexto(linea);
+        for (const string& palabra : palabras) {
+            mapaFrecuenciaPalabra[palabra]++;
+        }
+    }
+    
+    archivoEntrada.close();
+    
+    // Crear una lista de pares (palabra, frecuencia)
+    vector<pair<string, int>> paresPalabraFrecuencia;
+    for (const auto& entrada : mapaFrecuenciaPalabra) {
+        paresPalabraFrecuencia.push_back(entrada);
+    }
+    
+    // Ordenar la lista de pares usando bucket sort
+    ordenarPorFrecuencia(paresPalabraFrecuencia);
+    
+    // Escribir los resultados en el archivo de salida
+    ofstream archivoSalida(rutaArchivoSalida);
+    if (!archivoSalida.is_open()) {
+        cerr << "No se pudo abrir el archivo de salida.\n" << endl;
+        exit(EXIT_FAILURE);
+    }
+    
+    for (const auto& par : paresPalabraFrecuencia) {
+        archivoSalida << par.first << ", " << par.second << endl;
+    }
+    
+    archivoSalida.close();
+    
+    cout << "Proceso completado. Los resultados se han guardado en el archivo de salida." << endl;
+    
+    return;
+}
